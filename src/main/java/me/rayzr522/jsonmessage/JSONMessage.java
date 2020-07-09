590fea1d737e8e0a755e609efb490cc24b324603
Andre_601 <11576465+Andre601@users.noreply.github.com>
2020-07-09 09:09:21 +0200

    Update JSONMessage for 1.16 (#7)
    * Update JSONMessage for 1.16
    
    * Improve readme
    
    * Add hex color and font support
    
    * Add check for custom color in <1.16
    
    * Improvements and corrections
    
    * tiny spaces
    
    * Re-add old getColor and setColor methods
    
    * Update Color methods

--- src/main/java/me/rayzr522/jsonmessage/JSONMessage.java
+++ src/main/java/me/rayzr522/jsonmessage/JSONMessage.java
@@ -15,16 +15,14 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * This is a complete JSON message builder class. To create a new JSONMessage do
  * {@link #create(String)}
- *
+ * 
  * @author Rayzr
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
@@ -161,6 +159,11 @@ public class JSONMessage {
      * @param players The players you want to send this to
      */
     public void send(Player... players) {
+        if (ReflectionHelper.MAJOR_VER >= 16) {
+            ReflectionHelper.sendTextPacket(toString(), players);
+            return;
+        }
+
         ReflectionHelper.sendPacket(ReflectionHelper.createTextPacket(toString()), players);
     }
 
@@ -202,7 +205,52 @@ public class JSONMessage {
      * @return This {@link JSONMessage} instance
      */
     public JSONMessage color(ChatColor color) {
+        if (!color.isColor())
+            throw new IllegalArgumentException(color.name() + " is not a color.");
+        return color(color.name().toLowerCase(), ChatColor.WHITE);
+    }
+
+    /**
+     * Sets the color of the current message part.
+     * <br>If the provided color is a hex color ({@code #rrggbb}) but the major version of MC is older than 1.16 will this
+     * default to the color WHITE.
+     *
+     * @param color The color to set
+     * @return This {@link JSONMessage} instance
+     */
+    public JSONMessage color(String color) {
+        return color(color, ChatColor.WHITE);
+    }
+
+    /**
+     * Sets the color of the current message part.
+     * <br>If the provided color is a hex color ({@code #rrggbb}) but the major version of MC is older than 1.16 will the provided
+     * default ChatColor be used instead.
+     * 
+     * @param color The color to set
+     * @param def The default ChatColor to use, when MC version is older than 1.16
+     * @return This {@link JSONMessage} instance
+     */
+    public JSONMessage color(String color, ChatColor def) {
+        if (color.startsWith("#") && ReflectionHelper.MAJOR_VER < 16)
+            return color(def);
+
         last().setColor(color);
+        return this;
+    }
+
+    /**
+     * Sets the font of the current message part.
+     * <br>When this is used on versions older than 1.16 will this do nothing.
+     * 
+     * @param font The font to set
+     * @return This {@link JSONMessage} instance
+     */
+    public JSONMessage font(String font) {
+        if (ReflectionHelper.MAJOR_VER < 16)
+            return this;
+        
+        last().setFont(font);
         return this;
     }
 
@@ -247,6 +295,18 @@ public class JSONMessage {
      */
     public JSONMessage openURL(String url) {
         last().setOnClick(ClickEvent.openURL(url));
+        return this;
+    }
+
+    /**
+     * Copies the provided text to the Clipboard of the player.
+     * <br>When this is used on versions older than 1.15 will this default to {@link #suggestCommand(String) suggestCommand(String)}.
+     *
+     * @param text The text to copy
+     * @return This {@link JSONMessage} instance
+     */
+    public JSONMessage copyText(String text) {
+        last().setOnClick(ClickEvent.copyText(text));
         return this;
     }
 
@@ -421,10 +481,16 @@ public class JSONMessage {
         public JsonObject toJSON() {
             JsonObject obj = new JsonObject();
             obj.addProperty("action", action);
+            /*
+             * MC 1.16 changed "value" to "contents", but only for Hover events... Don't ask why.
+             * Since this lib only has tooltip and achievement can we simply check if action starts with "show_"
+             */
+            String valueType = (ReflectionHelper.MAJOR_VER >= 16 && action.startsWith("show_")) ? "contents" : "value";
+
             if (value instanceof JsonElement) {
-                obj.add("value", (JsonElement) value);
+                obj.add(valueType, (JsonElement) value);
             } else {
-                obj.addProperty("value", value.toString());
+                obj.addProperty(valueType, value.toString());
             }
             return obj;
         }
@@ -472,7 +538,7 @@ public class JSONMessage {
         }
 
         /**
-         * Suggests a command by putting inserting it in chat.
+         * Suggests a command by inserting it in chat.
          *
          * @param command The command to suggest
          * @return The {@link MessageEvent}
@@ -499,6 +565,20 @@ public class JSONMessage {
          */
         public static MessageEvent changePage(int page) {
             return new MessageEvent("change_page", page);
+        }
+
+        /**
+         * Copies the provided text to the clipboard of the player.
+         * <br>When used on versions older than 1.15 will this {@link #suggestCommand(String) suggest the text} instead.
+         *
+         * @param text The text to copy.
+         * @return The {@link MessageEvent}
+         */
+        public static MessageEvent copyText(String text) {
+            if (ReflectionHelper.MAJOR_VER < 15)
+                return suggestCommand(text);
+
+            return new MessageEvent("copy_to_clipboard", text);
         }
 
     }
@@ -674,6 +754,22 @@ public class JSONMessage {
 
         }
 
+        static void sendTextPacket(String message, Player... players) {
+            try {
+                for (Player player : players) {
+                    Class chatTypeClass = getClass("{nms}.ChatMessageType");
+                    Constructor<?> constructor = packetPlayOutChat.getConstructor(getClass("{nms}.IChatBaseComponent"), chatTypeClass, UUID.class);
+                    Object packet = constructor.newInstance(fromJson(message), Enum.valueOf(chatTypeClass, "CHAT"), player.getUniqueId());
+
+                    Object handler = player.getClass().getMethod("getHandle").invoke(player);
+                    Object playerConnection = handler.getClass().getField("playerConnection").get(handler);
+                    playerConnection.getClass().getMethod("sendPacket", getClass("{nms}.Packet")).invoke(playerConnection, packet);
+                }
+            } catch (IllegalArgumentException | NoSuchMethodException | NoSuchFieldException | IllegalAccessException | InvocationTargetException | InstantiationException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
         static Object createTitlePacket(String message) {
             if (!SETUP) {
                 throw new IllegalStateException("ReflectionHelper is not set up!");
@@ -816,12 +912,13 @@ public class JSONMessage {
      *
      * @author Rayzr
      */
-    public class MessagePart {
+    public static class MessagePart {
 
         private final List<ChatColor> styles = new ArrayList<>();
         private MessageEvent onClick;
         private MessageEvent onHover;
-        private ChatColor color;
+        private String color;
+        private String font;
         private String text;
 
         public MessagePart(String text) {
@@ -839,8 +936,8 @@ public class JSONMessage {
             JsonObject obj = new JsonObject();
             obj.addProperty("text", text);
 
-            if (color != null) {
-                obj.addProperty("color", color.name().toLowerCase());
+            if (color != null && !color.isEmpty()) {
+                obj.addProperty("color", color.toLowerCase());
             }
 
             for (ChatColor style : styles) {
@@ -855,6 +952,10 @@ public class JSONMessage {
                 obj.add("hoverEvent", onHover.toJSON());
             }
 
+            if (font != null) {
+                obj.addProperty("font", font);
+            }
+
             return obj;
 
         }
@@ -865,7 +966,7 @@ public class JSONMessage {
         public String toLegacy() {
             StringBuilder output = new StringBuilder();
             if (color != null) {
-                output.append(color.toString());
+                output.append(color);
             }
             styles.stream()
                     .map(ChatColor::toString)
@@ -905,16 +1006,43 @@ public class JSONMessage {
         /**
          * @return The color
          */
-        public ChatColor getColor() {
+        public String getColorValue() {
             return color;
+        }
+
+        /**
+         * @return The color
+         * 
+         * @deprecated Use {@link #getColorValue()} instead
+         */
+        @Deprecated
+        public ChatColor getColor() {
+            if(this.color.startsWith("#") && ReflectionHelper.MAJOR_VER < 16)
+                throw new IllegalStateException("Custom Hex colors can only be used in Minecraft 1.16 or newer!");
+
+            try {
+                return ChatColor.valueOf(this.color.toUpperCase());
+            } catch (Exception ex) {
+                return null;
+            }
+        }
+
+        /**
+         * @param color The color to set
+         * 
+         * @deprecated Use {@link #setColor(String)} instead
+         */
+        @Deprecated
+        public void setColor(ChatColor color) {
+            setColor(color.name().toLowerCase());
         }
 
         /**
          * @param color The color to set
          */
-        public void setColor(ChatColor color) {
-            if (!color.isColor()) {
-                throw new IllegalArgumentException(color.name() + " is not a color!");
+        public void setColor(String color) {
+            if (color == null || color.isEmpty()) {
+                throw new IllegalArgumentException("Color cannot be null!");
             }
             this.color = color;
         }
@@ -934,9 +1062,23 @@ public class JSONMessage {
                 throw new IllegalArgumentException("Style cannot be null!");
             }
             if (!style.isFormat()) {
-                throw new IllegalArgumentException(color.name() + " is not a style!");
+                throw new IllegalArgumentException(style.name() + " is not a style!");
             }
             styles.add(style);
+        }
+
+        /**
+         * @return The font used
+         */
+        public String getFont() {
+            return font;
+        }
+
+        /**
+         * @param font The font to use
+         */
+        public void setFont(String font) {
+            this.font = font;
         }
 
         /**
@@ -954,5 +1096,4 @@ public class JSONMessage {
         }
 
     }
-
 }
